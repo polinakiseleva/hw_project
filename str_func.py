806a1f179486f06b70c3a9543471f5a905ec487c
Polina <polinakiseleva4@gmail.com>
2023-02-27 14:28:23 +0200

    add func2 and docstr to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def func1(string):
+    """добавление докскринга"""
     return string.upper()
+
+
+def func2(string):
+    """Функция делает заглавной первую букву каждого слова"""
+    return string.title()
